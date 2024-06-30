e4b9315c842d7729c2333683b697402f055c8479
Katia Euzebio <santoseuzebiofabiano@gmail.com>
2024-06-30 17:04:17 -0300

    index, script, js

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ var emptySquare = "123456789";
 var playerSquares = "";
 var pcSquares = "";
 
-
 function getSquare(id) {
     return document.getElementById(`square${id}`);
 }
 
+function changeBackground(square) {
+    var square = getSquare(square);
+    square.style.backgroundColor = "lightgreen"
+}
+
 function validRow(squares, s1, s2, s3) {
     if (squares.includes(s1) && squares.includes(s2) && squares.includes(s3)) {
+        changeBackground(s1);
+        changeBackground(s2);
+        changeBackground(s3);
         return true;
     } else {
         return false
@@ -34,34 +41,45 @@ function play(playerNumber) {
     var playerSquare = getSquare(playerNumber);
     if (emptySquare.includes(playerNumber)) {
         playerSquare.innerHTML = '<img class="gameIcon" src="./images/o.png">'
-
         playerSquares += playerNumber;
+        playerSquare.removeAttribute("onclick")
+        validWinner(playerSquares)
         if (validWinner(playerSquares)) {
-            emptySquare == "";
-            console.log("Você ganhou")
+            emptySquare = "";
+            window.setTimeout(function () {
+                alertMessage("Você Ganhou!");
+            }, 500);
         } else {
             emptySquare = emptySquare.replace(playerNumber, "");
-        }
-    } if (!emptySquare.length == 0) {
-        pcNumber = "A"
-
-        do {
-            pcNumber = parseInt(Math.random() * 10);
-            var pcSquare = getSquare(pcNumber);
-            pcSquare.innerHTML = '<img class="gameIcon" src="./images/x.png">'
-
-            pcSquares += pcNumber;
-            console.log(pcSquares, "pcsq")
-            if (validWinner(playerSquares)) {
-                emptySquare == "";
-                console.log("O PC ganhou")
-                break;
-            } else {
-                emptySquare = emptySquare.replace(pcNumber, "");
+            if (emptySquare.length == 0) {
+                window.setTimeout(function () {
+                    alertMessage("Ninguém ganhou.");
+                }, 500);
             }
-        } while (emptySquare.includes(pcNumber));
-    }
-    console.log(emptySquare)
+        }
+    } if (emptySquare.length != 0) {
+        do {
+            pcNumber = parseInt(Math.random() * 9 + 1);
+        } while (!emptySquare.includes(pcNumber));
+        var pcSquare = getSquare(pcNumber);
+        pcSquare.innerHTML = '<img class="gameIcon" src="./images/x.png">'
+        pcSquares += pcNumber;
+        pcSquare.removeAttribute("onclick")
+        if (validWinner(pcSquares)) {
+            emptySquare = "";
+            window.setTimeout(function () {
+                alertMessage("O PC ganhou!");
+            }, 500);
+        } else {
+            emptySquare = emptySquare.replace(pcNumber, "");
+        }
 
+    }
 }
 
+function alertMessage(message) {
+    alert(`${message}`);
+    document.getElementById("row").style.display = "none";
+    document.getElementById("game").innerHTML = "<a href='index.html'><button>JOGAR NOVAMENTE</button></a>";
+
+}
